9315f2305366e0c477680f41b0f86a2d48ed7fec
Yohan Tov <yohantov@MacBook-Air-de-Yohan.local>
2024-05-13 10:12:16 +0200

    FIX : modif .env

--- my_quizz/src/Controller/RegistrationController.php
+++ my_quizz/src/Controller/RegistrationController.php
@@ -39,7 +39,7 @@ class RegistrationController extends AbstractController
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
-                    $form->get('password')->getData()
+                    $form->get('plainPassword')->getData()
                 )
             );
 
