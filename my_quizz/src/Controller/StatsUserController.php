f1579b770ad2ce36bd575800993f780ef47f4e2f
Yohan Tov <yohan.tov-pong@epitech.eu>
2024-05-21 21:04:49 +0200

    [ADD] Fonctionnalité stats finie, mise en page a moitié fait

--- my_quizz/src/Controller/StatsUserController.php
+++ my_quizz/src/Controller/StatsUserController.php
@@ -13,10 +13,63 @@ use Doctrine\ORM\EntityManagerInterface;
 class StatsUserController extends AbstractController
 {
     #[Route('/stats/user', name: 'app_stats_user')]
-    public function index(): Response
+    public function index(QuizzPassedRepository $quizzPassedRepository): Response
     {
+        $averagePoints = $quizzPassedRepository->findSumHarryPotter();
+        $totalUsers = $quizzPassedRepository->findTotalUserHarryPotter();
+
+        $averagePointsSigles = $quizzPassedRepository->findSumSigles();
+        $totalUsersSigles = $quizzPassedRepository->findTotalUserSigles();
+
+        $averagePointDefinition = $quizzPassedRepository->findSumDefinition();
+        $totalUsersDefinition = $quizzPassedRepository->findTotalUserDefinition();
+
+        $averagePointSpecialite = $quizzPassedRepository->findSumSpecialite();
+        $totalUsersSpecialite = $quizzPassedRepository->findTotalUserSpecialite();
+
+        $averagePointsSimpson = $quizzPassedRepository->findSumSimpson();
+        $totalUsersSimpson = $quizzPassedRepository->findTotalUserSimpson();
+
+        $averagePointsStargate = $quizzPassedRepository->findSumStargate();
+        $totalUsersStargate = $quizzPassedRepository->findTotalUserStargate();
+
+        $averagePointsNcis = $quizzPassedRepository->findSumNcis();
+        $totalUsersNcis = $quizzPassedRepository->findTotalUserNcis();
+
+        $averagePointsJeux = $quizzPassedRepository->findSumJeux();
+        $totalUsersJeux = $quizzPassedRepository->findTotalUserJeux();
+
+        $averagePointsProgrammation = $quizzPassedRepository->findSumProgrammation();
+        $totalUsersProgrammation = $quizzPassedRepository->findTotalUserProgrammation();
+
+        $averagePointsInformatique = $quizzPassedRepository->findSumInformatique();
+        $totalUsersInformatique = $quizzPassedRepository->findTotalUserInformatique();
+
+        
+
         return $this->render('stats_user/index.html.twig', [
             'controller_name' => 'StatsUserController',
+            'averagePoints' => $averagePoints,
+            'totalUsers' => $totalUsers,
+            'averagePointsSigles' => $averagePointsSigles,
+            'totalUsersSigles' => $totalUsersSigles,
+            'averagePointDefinition' => $averagePointDefinition,
+            'totalUsersDefinition' => $totalUsersDefinition,
+            'averagePointSpecialite' => $averagePointSpecialite,
+            'totalUsersSpecialite' => $totalUsersSpecialite,
+            'averagePointsSimpson' => $averagePointsSimpson,
+            'totalUsersSimpson' => $totalUsersSimpson,
+            'averagePointsStargate' => $averagePointsStargate,
+            'totalUsersStargate' => $totalUsersStargate,
+            'averagePointsNcis' => $averagePointsNcis,
+            'totalUsersNcis' => $totalUsersNcis,
+            'averagePointsJeux' => $averagePointsJeux,
+            'totalUsersJeux' => $totalUsersJeux,
+            'averagePointsProgrammation' => $averagePointsProgrammation,
+            'totalUsersProgrammation' => $totalUsersProgrammation,
+            'averagePointsInformatique' => $averagePointsInformatique,
+            'totalUsersInformatique' => $totalUsersInformatique,
+
         ]); 
     }
 }
