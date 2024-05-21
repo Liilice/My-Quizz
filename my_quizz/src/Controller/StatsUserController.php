fecd0f0f736c4f378a3f07a094444f89cfad2ee6
Yohan Tov <yohan.tov-pong@epitech.eu>
2024-05-21 16:38:00 +0200

    [ADD] Création classe, entity

--- my_quizz/src/Controller/StatsUserController.php
+++ my_quizz/src/Controller/StatsUserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\QuizzPassed;
+use App\Repository\QuizzPassedRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class StatsUserController extends AbstractController
 {
@@ -13,6 +17,6 @@ class StatsUserController extends AbstractController
     {
         return $this->render('stats_user/index.html.twig', [
             'controller_name' => 'StatsUserController',
-        ]);
+        ]); 
     }
 }
